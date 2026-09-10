7121a9d21e896ede8370435ab7559946041153c1
Hamid <hamidmsukardi@gmail.com>
2026-09-10 18:48:43 +0700

    riris

--- components/WeddingCover.tsx
+++ components/WeddingCover.tsx
@@ -68,7 +68,7 @@ export default function WeddingCover({ onOpen }: WeddingCoverProps) {
           letterSpacing: "2px",
           textShadow: "2px 2px 8px rgba(0,0,0,0.6)"
         }}>
-          Risis & Hamid
+          Riris & Hamid
         </h1>
       </div>
 
