1b12cb92db1d1eede6a222c7af683254f70cb6e6
Hamid <hamidmsukardi@gmail.com>
2026-09-11 22:02:34 +0700

    calendar

--- components/CalendarModel.tsx
+++ components/CalendarModel.tsx
@@ -1,18 +1,20 @@
+"use client";
+
+import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
+import * as THREE from "three";
 
 interface ModelProps {
   onSelect: () => void;
 }
 
 export default function CalendarModel({ onSelect }: ModelProps) {
-  const { scene } = useGLTF("/models/calendar/scene.gltf"); // Sesuaikan folder kalendermu
+  const { scene } = useGLTF("/models/calendar/scene.gltf");
 
   return (
-    <primitive 
-      object={scene} 
-      scale={16}
+    <group
       onClick={(e: any) => {
-        e.stopPropagation(); 
+        e.stopPropagation();
         onSelect();
       }}
       onPointerOver={(e: any) => {
@@ -22,6 +24,21 @@ export default function CalendarModel({ onSelect }: ModelProps) {
       onPointerOut={(e: any) => {
         document.body.style.cursor = "auto";
       }}
-    />
+    >
+      {/* Model utama Kalender */}
+      <primitive object={scene} scale={16} />
+
+      {/* Lingkaran Merah untuk Tanggal 15 */}
+      <mesh
+        // Sesuaikan koordinat position [X, Y, Z] dengan posisi angka 15 di model 3D kamu
+        position={[0, 0.2, 0.01]} 
+        // Sesuaikan rotation [X, Y, Z] agar sejajar dengan bidang kemiringan kalender
+        rotation={[0, 0, 0]} 
+      >
+        {/* RingGeometry(innerRadius, outerRadius, thetaSegments) */}
+        <ringGeometry args={[0.08, 0.1, 32]} />
+        <meshBasicMaterial color="#ff0000" side={THREE.DoubleSide} />
+      </mesh>
+    </group>
   );
 }
